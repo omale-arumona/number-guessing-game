42ae434c8bd13c0c96827a950566b8cb98f82448
Omale Arumona <omalearumona@gmail.com>
2024-05-25 12:24:09 +0100

    updated guess.js

--- guess.js
+++ guess.js
@@ -2,10 +2,10 @@ let inputValue = document.getElementById("guess");
 let submitBtn = document.getElementById("submit");
 let message = document.getElementById("message");
 let attempts = document.getElementById("attempts");
+let checkInput = document.querySelector(".checkInput");
 
 let attemptNum = 0;
 let randomNum = Math.floor(Math.random() * 100 + 1);
-console.log(randomNum);
 
 inputValue.value = "";
 let maxNumAttempt = 3;
@@ -13,6 +13,20 @@ let maxNumAttempt = 3;
 submitBtn.addEventListener("click", checkGuess);
 
 function checkGuess() {
+  let regex = /[a-zA-Z\s\W]/g;
+  let result = inputValue.value.search(regex);
+
+  if (!result) {
+    checkInput.textContent =
+      "Number can not contain special characters, spaces, alphabets, or decimals!";
+    checkInput.style.color = "red";
+    inputValue.style.border = "1px solid red";
+    return;
+  } else {
+    checkInput.textContent = "";
+    checkInput.style.color = "";
+    inputValue.style.border = "";
+  }
   const userValue = Number(inputValue.value);
   attemptNum++;
 
